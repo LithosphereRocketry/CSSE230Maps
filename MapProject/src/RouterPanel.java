7babe63e93df59ed0330abe4c819469d1cd1aad8
bhasinn <bhasinn@rose-hulman.edu>
2022-02-15 15:33:43 -0500

    JLabel Fix

--- MapProject/src/RouterPanel.java
+++ MapProject/src/RouterPanel.java
@@ -25,9 +25,17 @@ public class RouterPanel extends JPanel {
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
 		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 3.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		this.setLayout(gbl_panel_1);
+		
+		JLabel lblNewLabel = new JLabel("Start:");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.gridx = 4;
+		gbc_lblNewLabel.gridy = 1;
+		this.add(lblNewLabel, gbc_lblNewLabel);
 		
 		JComboBox comboBox = new JComboBox();
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
@@ -35,10 +43,15 @@ public class RouterPanel extends JPanel {
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox.gridx = 5;
 		gbc_comboBox.gridy = 1;
-		JLabel label = new JLabel();
-		label.setText("Start:");
-		this.add(label, gbc_comboBox);
 		this.add(comboBox, gbc_comboBox);
+		
+		JLabel lblNewLabel_1 = new JLabel("Destination:");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.gridx = 4;
+		gbc_lblNewLabel_1.gridy = 3;
+		this.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
@@ -46,9 +59,6 @@ public class RouterPanel extends JPanel {
 		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox_1.gridx = 5;
 		gbc_comboBox_1.gridy = 3;
-		JLabel label2 = new JLabel();
-		label2.setText("Destination:");
-		this.add(label2, gbc_comboBox_1);
 		this.add(comboBox_1, gbc_comboBox_1);
 	}
 }
