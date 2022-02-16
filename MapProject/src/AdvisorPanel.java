a7fa2d20ad40af29166719df40f616dfcfc4573b
bhasinn <bhasinn@rose-hulman.edu>
2022-02-16 08:42:51 -0500

    Fixed

--- MapProject/src/AdvisorPanel.java
+++ MapProject/src/AdvisorPanel.java
@@ -28,13 +28,13 @@ public class AdvisorPanel extends JPanel {
 		GridBagLayout gbl_panel_2 = new GridBagLayout();
 		gbl_panel_2.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel_2.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_panel_2.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 4.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_2.columnWeights = new double[]{1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, Double.MIN_VALUE};
 		gbl_panel_2.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		this.setLayout(gbl_panel_2);
 		
 		JFormattedTextField textField = new JFormattedTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 10);
+		gbc_textField.insets = new Insets(0, 0, 5, 25);
 		gbc_textField.anchor = GridBagConstraints.EAST;
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.gridx = 5;
@@ -42,7 +42,7 @@ public class AdvisorPanel extends JPanel {
 		this.add(textField, gbc_textField);
 		textField.setColumns(5);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Distance");
 		GridBagConstraints gbc_rdbtnNewRadioButton = new GridBagConstraints();
 		gbc_rdbtnNewRadioButton.insets = new Insets(0, 0, 0, 5);
 		gbc_rdbtnNewRadioButton.fill = GridBagConstraints.HORIZONTAL;
@@ -50,7 +50,7 @@ public class AdvisorPanel extends JPanel {
 		gbc_rdbtnNewRadioButton.gridy = 4;
 		this.add(rdbtnNewRadioButton, gbc_rdbtnNewRadioButton);
 		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("New radio button");
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Time");
 		GridBagConstraints gbc_rdbtnNewRadioButton_1 = new GridBagConstraints();
 		gbc_rdbtnNewRadioButton_1.insets = new Insets(0, 0, 0, 5);
 		gbc_rdbtnNewRadioButton.fill = GridBagConstraints.HORIZONTAL;
